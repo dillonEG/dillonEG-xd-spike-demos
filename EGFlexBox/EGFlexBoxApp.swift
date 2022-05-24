2dfddd3b735ba5b5a345d38285b1cac6962322a6
Dillon <dpetretti@expediagroup.com>
2022-05-24 14:55:28 -0500

    Initial Commit

---
+++ EGFlexBox/EGFlexBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  EGFlexBoxApp.swift
+//  EGFlexBox
+//
+//  Created by Dillon on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct EGFlexBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
