4084095dd8114325899529fed434e0131f8e5756
Dillon <dpetretti@expediagroup.com>
2022-05-24 16:39:01 -0500

    Add `Amenity` model and mock data

---
+++ EGFlexBox/Amenity.swift
@@ -0,0 +1,25 @@
+//
+//  Amenity.swift
+//  EGFlexBox
+//
+//  Created by Dillon on 5/24/22.
+//
+
+import Foundation
+
+struct Amenity: Identifiable {
+    let id = UUID()
+    let label: String
+    let glyphName: String
+}
+
+struct Mock {
+    static let amenities: [Amenity] = [
+        Amenity(label: "Washer & Dryer", glyphName: "snowflake"),
+        Amenity(label: "Fireplace", glyphName: "fireplace"),
+        Amenity(label: "Kitchen", glyphName: "oven"),
+        Amenity(label: "Hot tub", glyphName: "jacuzzi"),
+        Amenity(label: "Beach view", glyphName: "umbrella"),
+        Amenity(label: "Spa", glyphName: "lotus")
+    ]
+}
