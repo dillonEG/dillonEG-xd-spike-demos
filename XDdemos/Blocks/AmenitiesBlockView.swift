71cdeecfae177c7a8c4a383109617f3dbf055ca2
Dillon <dpetretti@expediagroup.com>
2022-05-25 14:47:05 -0500

    Adds custom `AmenityLabel` view modifier for text - Adds extension on `Color` for reusability

--- XDdemos/Blocks/AmenitiesBlockView.swift
+++ XDdemos/Blocks/AmenitiesBlockView.swift
@@ -13,7 +13,7 @@ struct AmenitiesBlockView: View {
     var body: some View {
         VStack(alignment: .leading, spacing: 14) {
             Text("Amenities")
-                .font(.custom("AvenirNext-Medium", size: 24))
+                .amenityTextLabel(.title)
             
             ForEach(amenities, id: \.self) { amenity in
                 AmenityView(amenity)
@@ -23,10 +23,11 @@ struct AmenitiesBlockView: View {
                 // do something
             } label: {
                 Text("See all")
-                    .font(.custom("AvenirNext-Regular", size: 14))
-                    .underline()
+                    .amenityTextLabel(accent: .textLink)
             }
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
+        .padding(.horizontal, 24)
     }
 }
 
@@ -43,13 +44,60 @@ struct AmenityView: View {
                 .resizable()
                 .renderingMode(.template)
                 .aspectRatio(contentMode: .fit)
-                .foregroundColor(Color(red: 52 / 255, green: 59 / 255, blue: 83 / 255, opacity: 1))
+                .foregroundColor(.neutral1_700)
                 .frame(width: 15, height: 15)
             
             Text(amenity.label)
-                .font(.custom("AvenirNext-Regular", size: 14))
+                .amenityTextLabel()
         }
     }
+}
+
+struct AmenityLabel: ViewModifier{
+    let style: FontStyle
+    let accent: AccentStyle
+    
+    enum FontStyle: String {
+        case title = "AvenirNext-Medium"
+        case description = "AvenirNext-Regular"
+    }
+    
+    enum AccentStyle {
+        case none
+        case textLink
+    }
+    
+    init(_ style: AmenityLabel.FontStyle, accent: AccentStyle = .none) {
+        self.style = style
+        self.accent = accent
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .font(.custom(style.rawValue, size: style == .description ? 14 : 24))
+            .foregroundColor(
+                accent == .textLink ? .accent1_500 : (
+                    style == .title ? .neutral1_900 : .neutral1_700
+                )
+            )
+    }
+}
+
+extension Text {
+    func amenityTextLabel(
+        _ size: AmenityLabel.FontStyle = .description,
+        accent: AmenityLabel.AccentStyle = .none
+    ) -> some View {
+        self
+            .underline(accent == .textLink ? true : false)
+            .modifier(AmenityLabel(size, accent: accent))
+    }
+}
+
+extension Color {
+    static let neutral1_900 = Color(red: 20 / 255, green: 29 / 255, blue: 56 / 255, opacity: 1)
+    static let neutral1_700 = Color(red: 52 / 255, green: 59 / 255, blue: 83 / 255, opacity: 1)
+    static let accent1_500 = Color(red: 54 / 255, green: 98 / 255, blue: 216 / 255, opacity: 1)
 }
 
 struct AmenitiesBlockView_Previews: PreviewProvider {
