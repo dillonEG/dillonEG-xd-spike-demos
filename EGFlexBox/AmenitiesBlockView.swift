de11b3617621402d7baef1bdd3d4aea7cdd4dfbd
Dillon <dpetretti@expediagroup.com>
2022-05-25 11:27:22 -0500

    Extracts Amenity view logic to new file

---
+++ EGFlexBox/AmenitiesBlockView.swift
@@ -0,0 +1,41 @@
+//
+//  AmenitiesBlockView.swift
+//  EGFlexBox
+//
+//  Created by Dillon on 5/25/22.
+//
+
+import SwiftUI
+
+struct AmenitiesBlockView: View {
+    let amenities: [Amenity] = Mock.amenities()
+    
+    var body: some View {
+        VStack {
+            ForEach(amenities) { item in
+                AmenityView(item)
+            }
+        }
+    }
+}
+
+struct AmenityView: View {
+    let amenity: Amenity
+    
+    init(_ amenity: Amenity) {
+        self.amenity = amenity
+    }
+    
+    var body: some View {
+        HStack {
+            Image(amenity.glyphName)
+            Text(amenity.label)
+        }
+    }
+}
+
+struct AmenitiesBlockView_Previews: PreviewProvider {
+    static var previews: some View {
+        AmenitiesBlockView()
+    }
+}
