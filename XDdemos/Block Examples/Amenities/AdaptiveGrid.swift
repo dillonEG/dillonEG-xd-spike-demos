f0fd81e74ec22c39053dd96b261431f947b0b3a8
Dillon <dpetretti@expediagroup.com>
2022-05-26 12:10:09 -0500

    Adds `FlexDirection` enum & property to grid init

--- XDdemos/Block Examples/Amenities/AdaptiveGrid.swift
+++ XDdemos/Block Examples/Amenities/AdaptiveGrid.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct AdaptiveGrid<Content>: View where Content: View {
+    let flexDirection: FlexDirection
     let minItemSize: CGFloat
     let gridContent: Content
     
-    init(minItemSize: CGFloat, @ViewBuilder content: () -> Content) {
+    init(flexDirection: FlexDirection, minItemSize: CGFloat, @ViewBuilder content: () -> Content) {
+        self.flexDirection = flexDirection
         self.minItemSize = minItemSize
         self.gridContent = content()
     }
@@ -25,11 +27,19 @@ struct AdaptiveGrid<Content>: View where Content: View {
     }
 }
 
+/// Based on ltr language (opposite for rtl?)
+enum FlexDirection {
+    case row // left --> right (default)
+    case rowReverse // left <-- right
+    case column // top --> bottom
+    case columnReverse // top <-- bottom
+}
+
 struct AdaptiveGrid_Previews: PreviewProvider {
     static var previews: some View {
         let amenities = Amenity.mock()
         
-        AdaptiveGrid(minItemSize: 190, content: {
+        AdaptiveGrid(flexDirection: .column, minItemSize: 190, content: {
             ForEach(amenities) { amenity in
                 AmenityItem(amenity)
             }
