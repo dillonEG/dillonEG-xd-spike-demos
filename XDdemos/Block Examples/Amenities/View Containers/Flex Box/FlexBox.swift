607c6c17210d34f176bd7843571c2ad02ad4557d
Dillon <dpetretti@expediagroup.com>
2022-05-30 17:19:54 -0500

    Adds new group & file for `FlexBox` implementations

---
+++ XDdemos/Block Examples/Amenities/View Containers/Flex Box/FlexBox.swift
@@ -0,0 +1,20 @@
+//
+//  FlexBox.swift
+//  XDdemos
+//
+//  Created by Dillon on 5/30/22.
+//
+
+import SwiftUI
+
+struct FlexBox: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlexBox_Previews: PreviewProvider {
+    static var previews: some View {
+        FlexBox()
+    }
+}
